cc052b8a06fbbdf0a883bf23615f6c2be270407c
Simon Martinelli <simon@72.services>
2020-06-18 10:10:26 +0200

    Initial Commit

---
+++ src/main/java/io/seventytwo/demo/GreetService.java
@@ -0,0 +1,16 @@
+package io.seventytwo.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+
+}
