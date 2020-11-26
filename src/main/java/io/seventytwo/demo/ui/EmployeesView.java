5b20be43e0b2d89a06bbfd049bbb0e2761066b88
Simon Martinelli <simon@72.services>
2020-11-26 18:27:27 +0100

    Added VaadinJooqUtils

--- src/main/java/io/seventytwo/demo/ui/EmployeesView.java
+++ src/main/java/io/seventytwo/demo/ui/EmployeesView.java
@@ -6,9 +6,6 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.provider.CallbackDataProvider;
 import com.vaadin.flow.data.provider.ConfigurableFilterDataProvider;
-import com.vaadin.flow.data.provider.Query;
-import com.vaadin.flow.data.provider.QuerySortOrder;
-import com.vaadin.flow.data.provider.SortDirection;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
@@ -17,15 +14,11 @@ import io.seventytwo.demo.database.tables.records.VEmployeeRecord;
 import org.apache.commons.lang3.StringUtils;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
-import org.jooq.Field;
-import org.jooq.OrderField;
 import org.jooq.impl.DSL;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static io.seventytwo.demo.database.tables.VEmployee.V_EMPLOYEE;
+import static io.seventytwo.vaadinjooq.util.VaadinJooqUtil.orderFields;
 
 @Route
 @PageTitle("Employees")
@@ -45,7 +38,7 @@ public class EmployeesView extends Div {
                 query -> dsl
                         .selectFrom(V_EMPLOYEE)
                         .where(query.getFilter().orElse(DSL.noCondition()))
-                        .orderBy(createOrderBy(query))
+                        .orderBy(orderFields(V_EMPLOYEE, query))
                         .offset(query.getOffset())
                         .limit(query.getLimit())
                         .fetchStream(),
@@ -97,12 +90,4 @@ public class EmployeesView extends Div {
         dataProvider.refreshItem(reloadedEmployee);
     }
 
-    private List<OrderField<?>> createOrderBy(Query<VEmployeeRecord, Condition> query) {
-        List<OrderField<?>> orderFields = new ArrayList<>();
-        for (QuerySortOrder sortOrder : query.getSortOrders()) {
-            Field<?> field = V_EMPLOYEE.field(sortOrder.getSorted());
-            orderFields.add(sortOrder.getDirection().equals(SortDirection.DESCENDING) ? field.desc() : field);
-        }
-        return orderFields;
-    }
 }
