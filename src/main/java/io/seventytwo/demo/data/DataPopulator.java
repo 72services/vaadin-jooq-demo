03aad03885902f8b7dc204c14ab1fc440a36c947
Simon Martinelli <simon@72.services>
2021-01-15 16:06:06 +0100

    Generate demo data with JavaFaker

--- src/main/java/io/seventytwo/demo/data/DataPopulator.java
+++ src/main/java/io/seventytwo/demo/data/DataPopulator.java
@@ -1,11 +1,10 @@
 package io.seventytwo.demo.data;
 
+import com.github.javafaker.Faker;
 import org.jooq.DSLContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 import static io.seventytwo.demo.database.tables.Department.DEPARTMENT;
 import static io.seventytwo.demo.database.tables.Employee.EMPLOYEE;
@@ -25,17 +24,20 @@ public class DataPopulator implements CommandLineRunner {
         dslContext.insertInto(DEPARTMENT).columns(DEPARTMENT.ID, DEPARTMENT.NAME).values(1, "IT").execute();
         dslContext.insertInto(DEPARTMENT).columns(DEPARTMENT.ID, DEPARTMENT.NAME).values(2, "HR").execute();
 
-        dslContext.insertInto(EMPLOYEE)
-                .columns(EMPLOYEE.ID, EMPLOYEE.NAME, EMPLOYEE.DATE_OF_BIRTH, EMPLOYEE.DEPARTMENT_ID)
-                .values(1, "Anna Muster", LocalDate.of(1981, 6, 21), 1)
-                .execute();
-        dslContext.insertInto(EMPLOYEE)
-                .columns(EMPLOYEE.ID, EMPLOYEE.NAME, EMPLOYEE.DATE_OF_BIRTH, EMPLOYEE.DEPARTMENT_ID)
-                .values(2, "Max Meier", LocalDate.of(1960, 11, 1), 1)
-                .execute();
-        dslContext.insertInto(EMPLOYEE)
-                .columns(EMPLOYEE.ID, EMPLOYEE.NAME, EMPLOYEE.DATE_OF_BIRTH, EMPLOYEE.DEPARTMENT_ID)
-                .values(3, "Kasimir Hofer", LocalDate.of(1970, 1, 3), 2)
-                .execute();
+        Faker faker = new Faker();
+
+        for (int i = 1; i < 101; i++) {
+            dslContext.insertInto(EMPLOYEE)
+                    .columns(EMPLOYEE.ID, EMPLOYEE.NAME, EMPLOYEE.DATE_OF_BIRTH, EMPLOYEE.DEPARTMENT_ID)
+                    .values(i, faker.name().fullName(), new java.sql.Date(faker.date().birthday().getTime()).toLocalDate(), 1)
+                    .execute();
+        }
+
+        for (int i = 101; i < 111; i++) {
+            dslContext.insertInto(EMPLOYEE)
+                    .columns(EMPLOYEE.ID, EMPLOYEE.NAME, EMPLOYEE.DATE_OF_BIRTH, EMPLOYEE.DEPARTMENT_ID)
+                    .values(i, faker.name().fullName(), new java.sql.Date(faker.date().birthday().getTime()).toLocalDate(), 2)
+                    .execute();
+        }
     }
 }
