1b0684cb6e0fb5e05a173a44cb37a747e794d13e
Simon Martinelli <simon@72.services>
2021-10-07 08:10:11 +0200

    Version update

--- src/main/java/io/seventytwo/demo/Application.java
+++ src/main/java/io/seventytwo/demo/Application.java
@@ -3,12 +3,13 @@ package io.seventytwo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
     }
 
 }
